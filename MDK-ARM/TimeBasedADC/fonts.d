09733ba011ca5afdcc5ca0a52fe5da89d6bdcdfe
Roman Kapitonov <romankapitonov.dev@gmail.com>
2020-04-24 01:30:30 +0300

    Initial.

---
+++ MDK-ARM/TimeBasedADC/fonts.d
@@ -0,0 +1,3 @@
+timebasedadc\fonts.o: ..\Drivers\ST7735\Src\fonts.c
+timebasedadc\fonts.o: ../Drivers/ST7735/Inc/fonts.h
+timebasedadc\fonts.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ MDK-ARM/TimeBasedADC/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+timebasedadc\startup_stm32f103xb.o: startup_stm32f103xb.s
